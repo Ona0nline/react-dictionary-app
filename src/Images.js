b57706e68321ca86e7eb3f4ad9923711b52fa4c2
Ona0nline <onasihle123@gmail.com>
2024-09-16 21:06:39 +0200

    Update Images.js
    Improved responsiveness and added more images

--- src/Images.js
+++ src/Images.js
@@ -2,19 +2,29 @@ import React from "react";
 
 export default function Images(props) {
   if (props.images) {
-    const imagesNum = props.images.slice(0, 5); // Only slice after confirming images exist
+    const imagesNum = props.images.slice(0, 3); // Only slice after confirming images exist
     return (
       <section>
-        {imagesNum.map(function (image, index) {
+        <div className="row">
+           {imagesNum.map(function (image, index) {
           return (
-            <img
+            <div className="col-4">
+              <a href={image.src.original} target="_blank" rel="noreferrer">
+                 <img
+                 className="img-fluid"
               key={index} // Add the key to avoid React warnings
               src={image.src.original} // Use the correct image URL
               alt={image.alt} // Alt text for accessibility
-              style={{ maxWidth: "100%", height: "auto", transform: "scale(0.5)" }} // Image styling
+              style={{ maxWidth: "100%", height: "auto", transform: "scale(0.3)" }} // Image styling
             />
+              </a>
+             
+            </div>
+            
           );
         })}
+        </div>
+       
       </section>
     );
   } else {
