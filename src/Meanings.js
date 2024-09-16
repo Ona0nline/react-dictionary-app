c894796619ed59c08821ec5516348e9b8b401930
Ona0nline <onasihle123@gmail.com>
2024-09-16 18:31:39 +0200

    Update Meanings.js
    Synonyms added

--- src/Meanings.js
+++ src/Meanings.js
@@ -2,13 +2,17 @@ import React from "react";
 import "./Meanings.css"
 
 export default function Meanings(props){
+  const displayedMeanings = props.meanings.slice(0, 5);
+
    return (
     <div className="meaning">
       <div className="meaning-box">
-        {props.meanings.length > 0 ? (
-        props.meanings.map((meaning, index) => (
+        {displayedMeanings.length > 0 ? (
+        displayedMeanings.map((meaning, index) => (
           <div key={index}>
-            <strong>Definition {index + 1}:</strong> {meaning.definition}
+            <strong>Definition {index + 1}:</strong> {meaning.definition}<br/>
+            <strong>Synonym {index + 1}:</strong> {meaning.synonyms[0]}<br/><br/>
+
           </div>
         ))
       ) : (
